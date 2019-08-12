12ed493aab9959536d6b014af0836ad8746fc452
Mick Evans <codemastermick@gmail.com>
2019-08-12 19:26:57 -0400

    refactor(footer): clean up tests to remove duplicated code

--- src/app/navigation/footer/footer.component.spec.ts
+++ src/app/navigation/footer/footer.component.spec.ts
@@ -25,49 +25,41 @@ describe("FooterComponent", () => {
   });
 
   it(`should have founding year`, () => {
-    const app = fixture.debugElement.componentInstance;
-    expect(app.foundingYear).not.toBeNull();
+    expect(component.foundingYear).not.toBeNull();
   });
 
   it(`should have author`, () => {
-    const app = fixture.debugElement.componentInstance;
-    expect(app.author).not.toBeNull();
+    expect(component.author).not.toBeNull();
   });
 
   it(`should have datestamp`, () => {
-    const app = fixture.debugElement.componentInstance;
-    expect(app.datestamp).not.toBeNull();
+    expect(component.datestamp).not.toBeNull();
   });
 
   it(`should have valid datestamp`, () => {
-    const app = fixture.debugElement.componentInstance;
-    expect(app.datestamp.length > 0).toBeTruthy();
+    expect(component.datestamp.length > 0).toBeTruthy();
   });
 
   it(`should have currentYear`, () => {
-    const app = fixture.debugElement.componentInstance;
-    expect(app.currentYear).not.toBeNull();
+    expect(component.currentYear).not.toBeNull();
   });
 
   it(`getRange should not be null`, () => {
-    const app = fixture.debugElement.componentInstance;
-    spyOn(app, "getRange");
+    spyOn(component, "getRange");
     fixture.detectChanges();
-    expect(app.getRange()).not.toBeNull();
+    expect(component.getRange()).not.toBeNull();
   });
 
   it(`getRange should return single year`, () => {
-    const app = fixture.debugElement.componentInstance;
-    spyOn(app, "getRange");
+    spyOn(component, "getRange");
     fixture.detectChanges();
-    expect(app.datestamp).toBe("2019");
+    expect(component.datestamp).toBe("2019");
   });
 
   it(`getRange should return a range`, () => {
-    const app = fixture.debugElement.componentInstance;
-    app.foundingYear = 2018;
-    app.datestamp = app.getRange();
+    component.foundingYear = 2018;
+    component.datestamp = component.getRange();
     fixture.detectChanges();
-    expect(app.datestamp).toBe("2018-2019");
+    expect(component.datestamp).toBe("2018-2019");
   });
 });
