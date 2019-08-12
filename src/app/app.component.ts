85e255c0f358e7c6bec9fc06cef89596dd741dc3
Mick Evans <codemastermick@gmail.com>
2019-08-12 15:41:03 -0400

    feat(app.component): change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-devops';
+  title = 'Codemaster Mick\'s portfolio';
 }
