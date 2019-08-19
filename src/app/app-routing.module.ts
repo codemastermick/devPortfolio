239c4f7c219e451841d999f8438f6dede8c2f0d0
Mick Evans <codemastermick@gmail.com>
2019-08-19 16:01:02 -0400

    refactor(app-routing): switch to double quotes to try and fix AOT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,21 +6,21 @@ import { PathResolveService } from "./shared/path-resolve-service.guard";
 const routes: Routes = [
   {
     path: paths.home,
-    loadChildren: () => import(`./pages/home/home.module`).then(m => m.HomeModule)
+    loadChildren: () => import("./pages/home/home.module").then(m => m.HomeModule)
   },
   {
     path: paths.about,
-    loadChildren: () => import(`./pages/about/about.module`).then(m => m.AboutModule)
+    loadChildren: () => import("./pages/about/about.module").then(m => m.AboutModule)
   },
   {
     path: paths.contact,
-    loadChildren: () => import(`./pages/contact/contact.module`).then(m => m.ContactModule)
+    loadChildren: () => import("./pages/contact/contact.module").then(m => m.ContactModule)
   },
   { path: "", redirectTo: "/home", pathMatch: "full" }, // Fallback to home if no route is found
   {
     path: "**",
     resolve: { path: PathResolveService },
-    loadChildren: () => import(`./pages/errors/not-found/not-found.module`).then(m => m.NotFoundModule)
+    loadChildren: () => import("./pages/errors/not-found/not-found.module").then(m => m.NotFoundModule)
   }
 ];
 
