14c608e9be6ca6b9b67a8d09c304468c15b3c799
Mick Evans <codemastermick@gmail.com>
2019-08-21 13:03:47 -0400

    chore: add spacing in code

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 })
 export class HeaderComponent implements OnInit {
   @Output() public sidenavToggle = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() { }
