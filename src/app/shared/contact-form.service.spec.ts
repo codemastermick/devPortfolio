1b1229d6189cf6d5bf8141ec3bd60c113c071a87
Mick Evans <codemastermick@gmail.com>
2019-08-31 20:54:16 -0400

    test(contact service): initial tests

--- src/app/shared/contact-form.service.spec.ts
+++ src/app/shared/contact-form.service.spec.ts
@@ -3,13 +3,30 @@ import { TestBed } from "@angular/core/testing";
 import { ContactFormService } from "./contact-form.service";
 import { HttpClientModule } from "@angular/common/http";
 
+const mock = {
+  name: "Demo Bob",
+  email: "demobob@fakemail.net",
+  message: "This is a better message, since it meets the minimum length requirements"
+};
+
 describe("ContactFormService", () => {
+  let service: ContactFormService;
+
   beforeEach(() => TestBed.configureTestingModule({
     imports: [HttpClientModule]
   }));
 
+  beforeEach(() => {
+    service = TestBed.get(ContactFormService);
+  });
+
   it("should be created", () => {
-    const service: ContactFormService = TestBed.get(ContactFormService);
     expect(service).toBeTruthy();
+  });
+
+  it("should upload a message", async () => {
+    const spy = spyOn(service, "uploadMessage");
+    await service.uploadMessage(mock.name, mock.email, mock.message);
+    expect(spy).toHaveBeenCalled();
   });
 });
