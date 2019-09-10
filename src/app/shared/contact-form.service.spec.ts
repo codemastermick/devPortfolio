8d4a0e43233c496cf0bf073ab99e268609b854a7
Mick Evans <codemastermick@gmail.com>
2019-09-10 11:02:30 -0400

    chore(contact service): remove dead import

--- src/app/shared/contact-form.service.spec.ts
+++ src/app/shared/contact-form.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from "@angular/core/testing";
 
 import { ContactFormService } from "./contact-form.service";
-import { HttpClientModule, HttpErrorResponse } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 
 const mock = {
   name: "Demo Bob",
